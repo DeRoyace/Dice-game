c574c9d949532be68d242dfcd02a1ed310c8fdef
Rohit Roy <creative.roy2003@gmail.com>
2022-06-25 14:40:22 +0530

    dice image source updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage1 = "gradientDicee/dice" + randomNumber1 + ".png";
 document.querySelector(".img1").setAttribute("src", randomImage1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImage2 = "gradientDicee/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomImage2);
 
 if (randomNumber1 > randomNumber2) {

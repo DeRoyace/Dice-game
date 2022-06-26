271c8ff763dee55aab7db82ad954e7b5ce9aa743
Rohit Roy <creative.roy2003@gmail.com>
2022-06-26 12:32:40 +0530

    play button sound added

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImage1 = "gradientDicee/dice" + randomNumber1 + ".png";
-document.querySelector(".img1").setAttribute("src", randomImage1);
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImage2 = "gradientDicee/dice" + randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute("src", randomImage2);
-
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins"
+function rollDice()
+{
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomImage1 = "gradientDicee/dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").setAttribute("src", randomImage1);
+    
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImage2 = "gradientDicee/dice" + randomNumber2 + ".png";
+    document.querySelector(".img2").setAttribute("src", randomImage2);
+    
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "Player 1 Wins"
+    }
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player 2 Wins"
+    }
+    else {
+        document.querySelector("h1").textContent = "Match Draw!"
+    }
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Player 2 Wins"
-}
-else {
-    document.querySelector("h1").textContent = "Match Draw!"
-}
+var flag = 0;
+document.querySelector(".btn").addEventListener("click", function ()
+{
+    flag = 1;
+    rollDice();
+    new Audio('roll_the_dice.mp3').play();
+});
+if(flag == 0)
+    rollDice();
